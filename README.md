f7738a14ef22df046a65b0deb59dddb8c5d3e481
jamal-mousavi72 <netmousavi@gmail.com>
2025-12-24 23:34:03 +0330

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Student Grade Manager 📚
+
+A simple Python command-line application to manage student grades using JSON as a database.
+
+## Features
+- Calculate GPA automatically.
+- Save student data to a JSON file.
+- Search for students by name.
+
+## How to Run
+1. Run `main.py` to add a student.
+2. Run `search_student.py` to find a student.
--- search_student.py
+++ search_student.py
@@ -4,4 +4,12 @@ import json
 search_name = input("Enter student name: ")
 with open("student.json", "r") as s:
     students = json.load(s)
-print(students)
+
+for s in students:
+    if s["name"].lower() == search_name.lower():
+
+        print(
+            f"Average is {s['average']} and Status is {s['status']}")
+        break
+else:
+    print("Student not found!")
